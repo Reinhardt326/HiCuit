03d2333fedd886df027a9243b51ca20ce32781d4
Reinhardt326 <315187183@qq.com>
2015-04-19 18:01:02 +0800

    update getVerifyCodePic

--- cloud/main.js
+++ cloud/main.js
@@ -63,22 +63,128 @@ AV.Cloud.define("getJwcCookie", function(request, response) {
 
 AV.Cloud.define("getVerifyCodePic", function(request, response) {
     var targetUrl = 'http://210.41.224.117/Login/xLogin/yzmDvCode.asp?k=552835&t=1429341700824';
-    var cookie = 'ASPSESSIONIDCSSTBCBD=KPMDGHMAKDNBFHAMKLONDNKF';
-    AV.Cloud.httpRequest({
-        url: targetUrl,
-        headers: {
-            'Cookie': cookie,
-            'Referer': 'http://210.41.224.117/Login/xLogin/Login.asp'
-        },
-        success: function(httpResponse) {
-            console.log(httpResponse.text);
-            console.log(httpResponse.headers);
+    var cookie = '';
+    var codeKey = '';
+    var datePrama;
+    AV.Cloud.run('getLoginCookieAndCodeKey', {}, {
+        success: function(data){
+            console.log("test");
+            var cookiesArray = data['cookies'];
+            for(var i = 0; i < cookiesArray.length; i++)
+            {
+                cookie += cookiesArray[i];
+            }
+            codeKey = data['codeKey'];
+            datePrama = data['datePrama'];
+            targetUrl = 'http://210.41.224.117/Login/xLogin/yzmDvCode.asp?k=' + codeKey + '&t=' +datePrama;
+
+            AV.Cloud.httpRequest({
+                url: targetUrl,
+                headers: {
+                    'Cookie': cookie,
+                    'Referer': 'http://210.41.224.117/Login/xLogin/Login.asp'
+                },
+                success: function(httpResponse) {
+                    console.log(httpResponse.text);
+                    console.log(httpResponse.headers);
 //            response.success('success');
-            response.success(httpResponse.data);
+                    response.success(httpResponse.text);
+                },
+                error: function(httpResponse) {
+                    console.error('Request failed with response code ' + httpResponse.status);
+                    response.success('Error ');
+                }
+            });
+        },
+        error: function(err){
+            console.log(err);
+            //处理调用失败
+        }
+    });
+    console.log("i'm running");
+});
+
+
+AV.Cloud.define("getLoginCookieAndCodeKey", function(request, response) {
+    var cheerio = require('cheerio');
+    var semicolonStr = ';';
+    AV.Cloud.httpRequest({
+        url: 'http://210.41.224.117/Login/xLogin/Login.asp',
+        success: function(httpResponse) {
+            var codeKey;
+            var $ = cheerio.load(httpResponse.text);
+            $("input").each(function (i, e) {
+                if($(e).attr('name') == 'codeKey')
+                {
+                    codeKey = $(e).attr('value');
+                }
+            });
+
+            var untreatedCookiesArray = httpResponse.headers['set-cookie'];
+            var handledCookieArray = new Array();
+
+            for(var i = 0; i < untreatedCookiesArray.length; i++)
+            {
+                var cookieStr = untreatedCookiesArray[i];
+                var lastIndex = cookieStr.indexOf(semicolonStr);
+                var handledCookieStr = cookieStr.slice(0,lastIndex);
+                handledCookieArray.push(handledCookieStr);
+            }
+            var dateParam = new Date().getTime();
+            var responseObj = {cookies:handledCookieArray,codeKey:codeKey,datePrama:dateParam};
+            response.success(responseObj);
         },
         error: function(httpResponse) {
             console.error('Request failed with response code ' + httpResponse.status);
             response.success('Error ');
         }
     });
+});
+
+AV.Cloud.define("loginPost", function(request, response) {
+    var cheerio = require('cheerio');
+    var semicolonStr = ';';
+    var cookie = '';
+    var codeKey = '';
+    AV.Cloud.run('getLoginPostInfo', {}, {
+        success: function(data){
+            var cookiesArray = data['cookies'];
+            for(var i = 0; i < cookiesArray.length; i++)
+            {
+                cookie += cookiesArray[i];
+            }
+            codeKey = data['codeKey'];
+            console.log(codeKey);
+        },
+        error: function(err){
+            console.log(err);
+            //处理调用失败
+        }
+    });
+//    AV.Cloud.httpRequest({
+//        method: 'POST',
+//        url: 'http://210.41.224.117/Login/xLogin/Login.asp',
+//        headers: {
+//            'Cookie': 'ASPSESSIONIDCQSSDCAC=PBNBFBCCFBNEDOLMPEBINEIF',
+//            'Referer': 'http://210.41.224.117/Login/xLogin/Login.asp'
+//        },
+//        body: {
+//            txtId: '2011051023',
+//            txtMM: 'Hn5201314',
+//            WinW: '1600',
+//            WinH: '860',
+//            verifycode: 'ejt',
+//            codeKey: '383190',
+//            Login: 'Check'
+//        },
+//        success: function(httpResponse) {
+//            console.log(httpResponse.text);
+//            console.log(httpResponse.headers);
+//            response.success('success');
+//        },
+//        error: function(httpResponse) {
+//            console.error('Request failed with response code ' + httpResponse.status);
+//            response.success('Error ');
+//        }
+//    });
 });
